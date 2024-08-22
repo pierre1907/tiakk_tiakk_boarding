848a825eb7fd76d6994ec66f0f223782828f1b39
Saint-Pierre KASSI <saint-pierre.kassi@ism.edu.sn>
2024-08-22 21:35:50 +0000

    initial commit 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'user',
+        pathMatch: 'full'
+    },
+    {
+        path: 'user',
+        loadChildren: ()=> import('./modules/users/users.module').then(m => m.UsersModule)
+    }
+];
---
+++ src/app/modules/users/components/first-login/first-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './first-login.component.html',
+  styleUrl: './first-login.component.css'
+})
+export class FirstLoginComponent {
+
+}
